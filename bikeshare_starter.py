10525fc4d8767cd4ad9eca0697b1923dd51f377e
Altaf <Komal.Altaf@ae.nestle.com>
2025-12-07 18:11:45 +0400

    Tidy imports and improve module docstring

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -2,6 +2,13 @@ import time
 import pandas as pd
 import numpy as np
 
+"""
+Bikeshare Data Analysis Script
+
+This script lets the user explore US bikeshare data for Chicago,
+New York City, and Washington.
+"""
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
